740fae8520de5d00091fb59e90df7949f4dd6c55
285480731 <53092331+285480731@users.noreply.github.com>
2024-10-16 23:47:29 +0800

    diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,7 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-
+#
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
