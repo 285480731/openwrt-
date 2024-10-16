515043f6c660bed7a95a69127083017946500040
285480731 <53092331+285480731@users.noreply.github.com>
2024-10-16 23:46:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,9 +15,9 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo '#src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default
-echo 'src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05' feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' feeds.conf.default
+echo '#src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
+echo 'src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05' >>feeds.conf.default
+echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main' >>feeds.conf.default
 #echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
