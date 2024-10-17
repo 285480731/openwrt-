9e3ce71a19b09c3b84902d90e7aeb399fc072029
285480731 <53092331+285480731@users.noreply.github.com>
2024-10-18 02:29:04 +0800

    diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,8 +16,6 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
-echo 'src-git other https://github.com/Lienol/openwrt-package.git;other' >>feeds.conf.default
-echo 'src-git lienol https://github.com/Lienol/openwrt-package.git;main' >>feeds.conf.default
-echo 'src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main' >>feeds.conf.default
+
 #echo 'src-git luci https://github.com/coolsnowwolf/luci' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
